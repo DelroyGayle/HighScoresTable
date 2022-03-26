f95a45b9cbd2b415f737661050ceff818490f63e
DelroyGayle <delroygayle01@gmail.com>
2022-03-26 14:20:53 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 import React from "react";
 import "./App.css";
 import allCountryScores from "./Scores.js";
 import ProduceScores from "./ProduceScores.js";
 
+
 // Sort the Country Names
 function countryNameSort(a,b) {
-  if (a.name < b.name)
-      return -1;
-
-  else if (a.name > b.name)
+  if (a.name < b.name) {
+               return -1;
+  } else if (a.name > b.name) {
       return 1;
-  else
+  } else {
       return 0;
+          }
 }
 
 const App = () => {
 
    // Sort the Country Names in alphabetical order - Note that the array is sorted in place
    allCountryScores.sort(countryNameSort);
-   
-  // render the result 
+
+  // render the result
   return (
-    <div className="flex-centre"> 
+    <div className="flex-centre">
 	    <div className="flexchild">
           <h1 className="title">High Scores per Country</h1>
           <ProduceScores theScores={allCountryScores} />
